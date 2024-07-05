5b850fe5017a905a3d81d45dcf3f98afce3c9bd5
Srikanta Ghosh <122613833+srikantaghosh@users.noreply.github.com>
2024-07-05 11:15:34 +0530

    Update DAG_data_ingestion.py

--- DAG_data_ingestion.py
+++ DAG_data_ingestion.py
@@ -6,11 +6,11 @@ import requests
 
 def fetch_and_dump_data_to_s3(url, s3_bucket, s3_key):
     response = requests.get(url)
-    data = response.json()
+    data = response.text
     
     s3 = S3Hook(aws_conn_id='my_aws_conn')
     s3.load_string(
-        string_data=json.dumps(data),
+        string_data=csv_data,
         key=s3_key,
         bucket_name=s3_bucket,
         replace=True
@@ -33,13 +33,13 @@ with DAG('fetch_and_dump_data', default_args=default_args, schedule_interval='@d
         'https://api.clubelo.com/YYYY-MM-DD'
     ]
     
-    s3_bucket = 'your-s3-bucket'
+    s3_bucket = 'apc-data-engineering'
     
     for i, url in enumerate(urls):
         task = PythonOperator(
             task_id=f'fetch_and_dump_{i}',
             python_callable=fetch_and_dump_data_to_s3,
-            op_args=[url, s3_bucket, f'data/provider_{i+1}.json'],
+            op_args=[url, s3_bucket, f'data/provider_{i+1}.csv'],
         )
         tasks.append(task)
 
